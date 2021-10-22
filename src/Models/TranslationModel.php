6f758fe9f74664b3e64ac85a9cdd79b87cf474b6
davesweb <davesweb@gmail.com>
2021-10-22 07:20:28 +0200

    Add translatable model, has translations trait and empty stub files

--- src/Models/TranslationModel.php
+++ src/Models/TranslationModel.php
@@ -3,7 +3,32 @@
 namespace Davesweb\LaravelTranslatable\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class TranslationModel extends Model
 {
+    public function translatesModel(): BelongsTo
+    {
+        return $this->belongsTo(
+            $this->translates,
+            $this->primaryKey,
+            $this->getTranslatesOwnerKey()
+        );
+    }
+    
+    public function getTranslatesModel(): Model
+    {
+        return $this->translates_model;
+    }
+    
+    private function getTranslatesObject(): Model
+    {
+        return resolve($this->translates);
+    }
+    
+    private function getTranslatesOwnerKey(): string
+    {
+        return Str::of($this->translates)->classBasename()->snake() . '_' . $this->getTranslatesObject()->primaryKey;
+    }
 }
--- src/Traits/HasTranslations.php
+++ src/Traits/HasTranslations.php
@@ -2,6 +2,49 @@
 
 namespace Davesweb\LaravelTranslatable\Traits;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Davesweb\LaravelTranslatable\Models\TranslationModel;
+
 trait HasTranslations
 {
+    protected array $translationsCache = [];
+
+    public function translations(): HasMany
+    {
+        return $this->hasMany($this->translation, $this->getTranslationForeignKey(), $this->getTranslationObject()->primaryKey);
+    }
+
+    public function getTranslations(): Collection
+    {
+        return $this->translations;
+    }
+
+    public function getTranslation(string $locale): ?TranslationModel
+    {
+        if (empty($this->translationsCache)) {
+            foreach ($this->getTranslations() as $translation) {
+                $this->translationsCache[$locale] = $translation;
+            }
+        }
+
+        return $this->translationsCache[$locale] ?? null;
+    }
+    
+    protected function getCurrentLocale(): string
+    {
+        return app()->getLocale();
+    }
+    
+    private function getTranslationObject(): Model
+    {
+        return resolve($this->translation);
+    }
+    
+    private function getTranslationForeignKey(): string
+    {
+        return Str::of(self::class)->classBasename()->snake() . '_' . $this->primaryKey;
+    }
 }
